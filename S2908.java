f193920ffef6b18f51a73301910dacc66d0a93d3
parkkanghwi <164290928+parkkanghwi@users.noreply.github.com>
2025-07-02 23:28:00 +0900

    Add files via upload
    2908 with java

--- S2908.java
+++ S2908.java
@@ -1,10 +1,22 @@
 package first;
 
-public class S2908 {
-
+import java.util.Scanner;
+ 
+public class S2908{
+ 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+ 
+		Scanner in = new Scanner(System.in);
+        
+		int A = in.nextInt();
+		int B = in.nextInt();
+        
+        in.close();
+        
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.print(A > B ? A : B);
+	
 	}
-
 }
